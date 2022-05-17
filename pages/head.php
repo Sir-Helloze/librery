1e7c181b7273d2f2c100d731d3560fb0ff6b4cb2
Sir-Helloze <babywalk@mail.ru>
2022-05-17 15:15:43 +0400

    Create head.php

---
+++ pages/head.php
@@ -0,0 +1,16 @@
+<div class="header">
+        <div class="ico"><a href="#" ><img src="images/libraryLogo.png" alt="logo" style="width: 80px; height: 80px;"></a></div>
+    </div>
+    <nav id="menuVert">
+        <ul>
+            <li><a href="index.php"><div class="icon_n"><i class="fas fa-home"></i></div><span>HOME</span></a></li>
+            <li><a href="later.php"><div class="icon_n"><i class="fas fa-bookmark"></i></div><span>FAVORITES</span></a></li>
+            <li><a href="#"><div class="icon_n"><i class="fas fa-book-open"></i></div><span>BOOK</span></a>
+            <ul>
+                <li><a href="addBook.php"><div class="icon_n"><i class="fas fa-plus-circle"></i></div><span>ADD BOOK</span></a></li>  
+                <li><a href="cart.php"><div class="icon_n"><i class="fas fa-book"></i></div><span>READ A BOOK</span></a></li>
+            </ul>
+            </li>
+        </ul>
+    </nav>
+    <div class="menu"><div class="footer">library <br> powered by <a href="index.php" id="footHome"><span>HOME</span></a></div></div>
