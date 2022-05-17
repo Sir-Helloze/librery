bde7a45fb4edbc389396164d71f1c80a0948c404
Sir-Helloze <babywalk@mail.ru>
2022-05-17 15:15:35 +0400

    Create later.php

---
+++ later.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>libraryFavorites</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+<?php include "pages/head.php"?>
+<a href="index.html">На главную</a>
+<a href="cart.html">Ваша корзина</a>
+<main>
+   <h1>Избранные товары:</h1>
+    <div class="goods-out"></div>
+</main>
+<footer></footer>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/later.js"></script>
+</body>
+</html>
