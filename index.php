f763a8490a7910113612d012ddbe48a604733dcc
Sir-Helloze <babywalk@mail.ru>
2022-05-17 15:15:16 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>library</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+
+<?php include "pages/head.php"?>
+<a href="cart.html">Ваша корзина</a>
+<a href="later.html">Избранные товары</a>
+<main>
+    <h1>Book selection</h1>
+    <div class="goods-out"></div>
+</main>
+
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
