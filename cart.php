97f17698543bfd90f48bc0c4faa89df82b0e9be2
Sir-Helloze <babywalk@mail.ru>
2022-05-17 15:14:44 +0400

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>libraryAdd</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<?php include "pages/head.php"?>
+<a href="index.html">На главную</a>
+<a href="later.html">Избранные товары</a>
+<main>
+<h1>your books</h1>
+    <div class="main-cart"></div>
+</main>
+<footer></footer>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/cart.js"></script>
+</body>
+</html>
