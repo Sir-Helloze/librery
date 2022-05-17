f60e36737c6970705dc896d34c98e8df01c706ae
Sir-Helloze <babywalk@mail.ru>
2022-05-17 15:14:57 +0400

    Create goods.php

---
+++ goods.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>libraryDescription</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+
+<?php include "pages/head.php"?>
+<a href="cart.html">Ваша корзина</a>
+<a href="later.html">Избранные товары</a>
+<main>
+    <div class="goods-out"></div>
+</main>
+<footer></footer>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/goods.js"></script>
+</body>
+</html>
